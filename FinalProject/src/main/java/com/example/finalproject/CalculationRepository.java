0740cb5236c7160a5f0c5c8da72f18e46451ff6a
brutalll03 <103244831+brutalll03@users.noreply.github.com>
2024-02-13 17:51:32 +0100

    Delete FinalProject directory

--- FinalProject/src/main/java/com/example/finalproject/CalculationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface CalculationRepository extends JpaRepository<Calculation, Long> {
-}
